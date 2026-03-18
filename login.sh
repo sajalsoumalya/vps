a275540e5ab1aa7d75982a7cb2ac8baa621b1360
Soumalya Chowdhury <soumalya9862@gmail.com>
2026-03-18 12:02:19 +0530

    Change credentials in login.sh
    Updated username and password in login.sh

--- login.sh
+++ login.sh
@@ -2,5 +2,5 @@
 echo ..........................................................
 echo IP:
 curl -s http://localhost:4040/api/tunnels | grep -o '"public_url":"[^"]*' | sed 's/"public_url":"//'
-echo Username: runneradmin
-echo Password: P@ssw0rd!
+echo Username: soumalya
+echo Password: soumalya
